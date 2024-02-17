22e8d01db956a656f430af0332a72d954a3ca3e4
AndreMartins21 <andrezinhomm17@gmail.com>
2024-02-16 22:25:29 -0300

    fix(c): using hash_map to solve problem 1 (two-sum)

--- C/leet_code_1.c
+++ C/leet_code_1.c
@@ -6,26 +6,57 @@
  * Note: The returned array must be malloced, assume caller calls free().
  * Link: https://leetcode.com/problems/two-sum/
  */
-int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
-    int i, j;
-    int* array_idx = (int*) malloc(sizeof(int)*2);
-    
-    for (i=0; i<numsSize; i++){
-        if (nums[i] > target && nums[i] > 0) continue;
 
-        for (j=0; j<numsSize; j++){
-            if(j==i) continue;
-            
-            if (nums[j] + nums[i] == target){
-                array_idx[0] = j;
-                array_idx[1] = i;
-                break;
-            }
-        }
+
+typedef struct HashMapNum {
+    int idx;
+    int value;
+} HashMapNum;
+
+
+HashMapNum* number_is_in_hash_map(HashMapNum* hash_map, int v, int size) {
+    int i;
+    for (i = 0; i < size; i++) {
+        if (hash_map[i].value == v)
+            return &hash_map[i];
+    }
+    return NULL;
+}
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    int* result = (int*)malloc(sizeof(int) * 2);
+    HashMapNum* hash_map = (HashMapNum*) malloc(sizeof(HashMapNum) * numsSize);
+    
+    if (!result || !hash_map) {
+        *returnSize = 0;
+        return NULL;
     }
 
-    *returnSize = 2;
-    return array_idx;
+    int i;
+    for (i = 0; i < numsSize; i++) {
+        int diff = target - nums[i];
+        HashMapNum* found = number_is_in_hash_map(hash_map, diff, numsSize);
+        
+        if (found != NULL && found->idx != i) {
+            result[0] = found->idx;
+            result[1] = i;
+
+            *returnSize = 2;
+            free(hash_map);
+            
+            return result;
+        }
+        
+        if (number_is_in_hash_map(hash_map, nums[i], numsSize) == NULL) {
+            hash_map[i].idx = i;
+            hash_map[i].value = nums[i];
+        }
+    }
+    
+    *returnSize = 0;
+    free(hash_map);
+    free(result);
+    return NULL;
 }
 
 void print_array(int *array, int len_array){
@@ -40,8 +71,8 @@ void print_array(int *array, int len_array){
 
 
 void main(){
-    int nums[] = {2,4,11,3};
-    int target = 6;
+    int nums[] = {2,7,11,15};
+    int target = 18;
     int numsSize = sizeof(nums) / sizeof(nums[0]);
     int returnSize;
     int* return_idx;
